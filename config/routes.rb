d1c525840c2c68df75028434d19f9f7df3fc384c
tulakom <tula775@hotmail.com>
2016-11-12 14:27:04 +0700

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  Rails.application.routes.draw do
+ root 'application#hello'
 end
